1e58046d6f1ded2ba6c816c72aaa563f7ab7b537
Renier Joubert <rjoube01@students.cs.ubc.ca>
2026-03-10 16:46:04 -0700

    ignore

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "renierj.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
